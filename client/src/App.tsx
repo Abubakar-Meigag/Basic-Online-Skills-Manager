5c9e4240fbefdf762720119e8f590ffdf75b82f4
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-07-19 16:56:24 +0100

    install client frameword and dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Basic Online Skills Manager</h1>
+        <p>
+          This is a simple CDM application .
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default App
