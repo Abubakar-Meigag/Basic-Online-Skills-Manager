d44ca4c544cb12f7c33e850a928b0e6ac4f112f1
Jamal Laqdiem <jlaqdiem@gmail.com>
2026-07-20 17:02:50 +0100

    chore: configure tailwind v4 and clean up css boilerplate.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
-import './App.css'
-
 function App() {
-
   return (
-    <>
-      <section id="center">
-        <h1>Basic Online Skills Manager</h1>
-        <p>
-          This is a simple CRM application .
-        </p>
+    <div className="flex flex-col gap-[25px] justify-center items-center flex-grow min-h-screen">
+      <section className="text-center">
+        <h1 className="text-3xl font-bold">Basic Online Skills Manager</h1>
+        <p className="text-gray-600 mt-2">This is a simple CRM application.</p>
       </section>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
