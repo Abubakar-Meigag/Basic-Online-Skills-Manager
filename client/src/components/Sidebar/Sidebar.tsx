a01f4665b12096788a395c19525bc75c38b97418
Craig D'Silva <craigdsilva@proton.me>
2026-07-27 17:27:55 +0100

    Add elements conditionally

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,36 @@
 const Sidebar = () => {
+  const userType: string = "";
+
   return (
-    <div>
+    <div className="sidebar">
       <img src="/src/assets/CYF-logo.png" width="256" height="256" />
+
+      {userType === "commercial" && (
+        <nav>
+          <ul>
+            <li>Courses</li>
+            <li>Request New Course</li>
+          </ul>
+        </nav>
+      )}
+      {userType === "outreach" && (
+        <nav>
+          <ul>
+            <li>Find Opportunities</li>
+            <li>Requested Courses</li>
+          </ul>
+        </nav>
+      )}
+      {userType === "cyf-staff" && (
+        <nav>
+          <ul>
+            <li>Request Pipeline</li>
+            <li>Manage Partners</li>
+            <li>Audit Log</li>
+          </ul>
+        </nav>
+      )}
+      <button type="button">Logout</button>
     </div>
   );
 };
