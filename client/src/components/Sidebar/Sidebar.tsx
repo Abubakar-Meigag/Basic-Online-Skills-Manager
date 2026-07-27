3df3d0bef5420342fc8295e0de3dfc3e89fc02e2
Craig D'Silva <craigdsilva@proton.me>
2026-07-27 16:31:19 +0100

    Add CYF logo

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,9 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <img src="/src/assets/CYF-logo.png" width="256" height="256" />
+    </div>
+  );
 };
 
 export default Sidebar;
