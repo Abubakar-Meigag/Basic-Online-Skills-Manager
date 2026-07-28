bc98d617a82305560c68a005f989f51d306265d6
Craig D'Silva <craigdsilva@proton.me>
2026-07-28 08:29:40 +0100

    Add comment explaining how to test component

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -2,8 +2,7 @@ import { NavLink } from "react-router";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  const userType: string = "commercial";
-  //  /dashboard/commercial-partner
+  const userType: string = "commercial"; // Change to see view from a different user. Accepts strings "commercial", "outreach" & "cyf-staff"
 
   return (
     <div className="sidebar p-5 shrink-0 sticky top-0 self-start h-screen">
