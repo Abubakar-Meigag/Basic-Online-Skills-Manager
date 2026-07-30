a2545fc79431c77ee6260a65a650c3fdcca78b8c
Jamal Laqdiem <jlaqdiem@gmail.com>
2026-07-30 13:24:49 +0100

    Magic Link Login  (#31)
    * feat: add MagicLinkLogin component and unit tests.
    
    * updated the logo with the cyf  link img.
    
    * Adding the path to login page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MagicLinkLogin from "./components/MagicLinkLogin";
 function App() {
   return (
-    <div className="flex flex-col gap-[25px] justify-center items-center flex-grow min-h-screen">
-      <section className="text-center">
-        <h1 className="text-3xl font-bold">Basic Online Skills Manager</h1>
-        <p className="text-gray-600 mt-2">This is a simple CRM application.</p>
-      </section>
-    </div>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/login" element={<MagicLinkLogin />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
