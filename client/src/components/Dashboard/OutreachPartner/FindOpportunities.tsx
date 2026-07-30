3017b2c7b6629fabe449f8b633f82876b1807f45
Craig D'Silva <craigdsilva@proton.me>
2026-07-30 19:52:21 +0100

    Add table

--- client/src/components/Dashboard/OutreachPartner/FindOpportunities.tsx
+++ client/src/components/Dashboard/OutreachPartner/FindOpportunities.tsx
@@ -1,7 +1,7 @@
 const FindOpportunities = () => {
   return (
-    <div className="find-opportunities ml-64 p-6 min-h-screen">
-      <div className="discovery-header flex justify-between">
+    <div className="find-opportunities">
+      <div className="discovery-header flex justify-between mt-10 mb-20 mx-12">
         <p>Available Opportunities</p>
         <select name="filter by city" id="filter-by-city">
           <option value="select-a-city">Select a city...</option>
@@ -10,6 +10,32 @@ const FindOpportunities = () => {
           <option value="manchester">Manchester</option>
         </select>
       </div>
+      <table className="find-opportunities-table table-auto w-full text-left">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Commercial Partner</th>
+            <th>Location</th>
+            <th>Trainee Target</th>
+            <th>Duration</th>
+            <th>Deadline</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>REQ-0051</td>
+            <td>Capgemini</td>
+            <td>London</td>
+            <td>24</td>
+            <td>3 weeks</td>
+            <td>15 Aug 2026</td>
+            <td>Open</td>
+            <td>View Opportunity Details</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
