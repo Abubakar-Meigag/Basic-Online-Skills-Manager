0a9cf6e46e67de74dfcc7ceddc6a29484e48158c
Craig D'Silva <craigdsilva@proton.me>
2026-07-31 13:43:40 +0100

    Fix MagicLinkLogin import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RoleDashboardView from "./views/RoleDashboardView";
+import MagicLinkLogin from "./components/MagicLinkLogin";
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
@@ -11,6 +12,6 @@ function App() {
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
-}
+};
 
 export default App;
