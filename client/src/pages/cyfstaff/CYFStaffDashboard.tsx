d6b307b650e1e230117602a081d4fc4702975a5d
Craig D'Silva <craigdsilva@proton.me>
2026-08-08 17:39:05 +0100

    Fetch courses

--- client/src/pages/cyfstaff/CYFStaffDashboard.tsx
+++ client/src/pages/cyfstaff/CYFStaffDashboard.tsx
@@ -1,4 +1,25 @@
+import { useCallback, useEffect, useState } from "react";
+import CYFStaffTable from "../../components/CYFStaffTable/CYFStaffTable";
+
 const CYFStaffDashboard = () => {
+  const [courses, setCourses] = useState({});
+
+  const getCourses = useCallback(async () => {
+    try {
+      const res = await fetch(
+        "https://bosm-backend.trainees.hosting.cyf.academy/course-pipeline",
+      );
+      const data = await res.json();
+      setCourses(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getCourses();
+  }, [getCourses]);
+
   return (
     <div className="request-pipeline">
       <div className="flex justify-between mt-10 mb-20 mx-12">
