9e81154cfcba0e2f9145822949406685dd98fba8
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-08-19 15:15:25 +0100

    update remove text-center

--- client/src/lib/constants/statusStyles.ts
+++ client/src/lib/constants/statusStyles.ts
@@ -2,17 +2,17 @@ import type { CourseStatus } from "../../data/dataType";
 
 export const statusStyles: Record<CourseStatus, string> = {
   request_pending:
-    "bg-amber-100 text-center text-amber-600 border-2 min-w-[120px] text-center border-amber-300 rounded-lg",
+    "bg-amber-100 text-center text-amber-600 border-2 min-w-[120px]border-amber-300 rounded-lg",
   request_open:
-    "bg-slate-100 text-center text-slate-800 border-2 min-w-[120px] text-center border-slate-400 rounded-lg",
+    "bg-slate-100 text-center text-slate-800 border-2 min-w-[120px] border-slate-400 rounded-lg",
   request_claimed:
-    "bg-yellow-50 text-center text-yellow-700 border-2 min-w-[120px] text-center border-yellow-500 rounded-lg",
+    "bg-yellow-50 text-center text-yellow-700 border-2 min-w-[120px] border-yellow-500 rounded-lg",
   request_confirmed:
-    "bg-green-50 text-center text-green-700 border-2 min-w-[120px] text-center border-green-300 rounded-lg",
+    "bg-green-50 text-center text-green-700 border-2 min-w-[120px] border-green-300 rounded-lg",
   course_completed:
-    "bg-stone-100 text-center text-stone-700 border-2 min-w-[120px] text-center border-stone-400 rounded-lg",
+    "bg-stone-100 text-center text-stone-700 border-2 min-w-[120px] border-stone-400 rounded-lg",
   course_running:
-    "bg-blue-100 text-center text-blue-700 border-2 min-w-[120px] text-center border-blue-300 rounded-lg",
+    "bg-blue-100 text-center text-blue-700 border-2 min-w-[120px] border-blue-300 rounded-lg",
   request_cancelled:
-    "bg-stone-50 text-center text-gray-700 border-2 min-w-[120px] text-center border-gray-400 rounded-lg",
+    "bg-stone-50 text-center text-gray-700 border-2 min-w-[120px] border-gray-400 rounded-lg",
 };
